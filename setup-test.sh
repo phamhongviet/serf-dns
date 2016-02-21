551ece84ec46161cb80a682b18cd105dbae86793
Viet Pham <phamhongviet52@gmail.com>
2016-02-21 19:59:47 +0700

    add a real docker image

--- setup-test.sh
+++ setup-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# serf docker image
-SERF=serf
+SERF=quay.io/phamhongviet/serf
 
-LEAD=`docker run -d $SERF agent -tag dc=cali -tag srv=foo`
-LEAD_IP=`docker inspect -f '{{.NetworkSettings.IPAddress}}' $LEAD`
-docker run -d $SERF agent -tag dc=cali -tag srv=foo -join $LEAD_IP
-docker run -d $SERF agent -tag dc=cali -tag srv=bar -join $LEAD_IP
-docker run -d $SERF agent -tag dc=oreg -tag srv=bar -join $LEAD_IP
-docker run -d $SERF agent -tag dc=oreg -tag srv=foo -join $LEAD_IP
-docker run -d -p 127.0.0.1:7373:7373 $SERF agent -join $LEAD_IP -rpc-addr '0.0.0.0:7373'
+LEADER=`docker run -d -p 127.0.0.1:7373:7373 $SERF agent -rpc-addr '0.0.0.0:7373'`
+LEADER_IP=`docker inspect -f '{{.NetworkSettings.IPAddress}}' $LEADER`
+
+docker run -d $SERF agent -tag dc=cali -tag srv=foo -join $LEADER_IP
+docker run -d $SERF agent -tag dc=cali -tag srv=foo -join $LEADER_IP
+docker run -d $SERF agent -tag dc=cali -tag srv=bar -join $LEADER_IP
+docker run -d $SERF agent -tag dc=oreg -tag srv=bar -join $LEADER_IP
+docker run -d $SERF agent -tag dc=oreg -tag srv=foo -join $LEADER_IP
