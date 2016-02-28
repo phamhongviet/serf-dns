a782356a1f3f0dda877bc347b5b16a603d965ee4
Viet Pham <phamhongviet52@gmail.com>
2016-02-28 18:46:09 +0700

    test parse domain name

--- parse_test.go
+++ parse_test.go
@@ -5,4 +5,18 @@ import (
 )
 
 func TestParseDomainName(t *testing.T) {
+	domainNameSample := "foo.srv.cali.dc.serf."
+	expectedSerfFilter := serfFilter{
+		Tags: map[string]string{
+			"srv": "foo",
+			"dc":  "cali",
+		},
+		Status: "alive",
+	}
+	resultSerfFilter := parseDomainName(domainNameSample)
+	// TODO: implement compare serf filters
+	ok := expectedSerfFilter.Compare(resultSerfFilter)
+	if !ok {
+		t.Errorf("Failed to parse domain name %s", domainNameSample)
+	}
 }
--- serf.go
+++ serf.go
@@ -5,3 +5,7 @@ type serfFilter struct {
 	Status string
 	Name   string
 }
+
+func (sf1 *serfFilter) Compare(sf2 serfFilter) bool {
+	return true
+}
