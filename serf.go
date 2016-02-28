82a9dc3d125afaea55ebeb500d7227332df05d8a
Viet Pham <phamhongviet52@gmail.com>
2016-02-28 20:35:41 +0700

    implement serfFilter Compare

--- serf.go
+++ serf.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"reflect"
+	"strings"
+)
+
 type serfFilter struct {
 	Tags   map[string]string
 	Status string
@@ -7,5 +12,13 @@ type serfFilter struct {
 }
 
 func (sf1 *serfFilter) Compare(sf2 serfFilter) bool {
-	return true
+	if strings.Compare(sf1.Status, sf2.Status) != 0 {
+		return false
+	}
+
+	if strings.Compare(sf1.Name, sf2.Name) != 0 {
+		return false
+	}
+
+	return reflect.DeepEqual(sf1.Tags, sf2.Tags)
 }
