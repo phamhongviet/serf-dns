affdb16a2b6b14f73d6683f84e9d7d6c1ace91d5
Viet Pham <phamhongviet52@gmail.com>
2016-03-03 07:20:31 +0700

    add test for connect to serf agent

--- serf_test.go
+++ serf_test.go
@@ -38,3 +38,37 @@ func TestSerfFilterCompare(t *testing.T) {
 		t.Errorf("SerfFilter Compare return unexpected result.")
 	}
 }
+
+func TestConnectSerfAgentExpectingFailure(t *testing.T) {
+	client, err := connectSerfAgent("127.0.0.1:55555")
+	defer func() {
+		if client != nil {
+			client.Close()
+		}
+	}()
+
+	if err == nil {
+		t.Errorf("Connect to wrong address return no error")
+	}
+
+	if client != nil {
+		t.Errorf("Connect to wrong address return opened client")
+	}
+}
+
+func TestConnectSerfAgentExpectingSuccess(t *testing.T) {
+	client, err := connectSerfAgent(defaultSerfRPCAddress)
+	defer func() {
+		if client != nil {
+			client.Close()
+		}
+	}()
+
+	if err != nil {
+		t.Errorf("Connect to default address return no error. Did you setup test environment?")
+	}
+
+	if client == nil {
+		t.Errorf("Connect to default address return opened client. Did you setup test environment?")
+	}
+}
