462d9693f2e4b23cece6e64d94db27c4718f8060
Viet Pham <phamhongviet52@gmail.com>
2016-03-13 12:14:07 +0700

    only output running containers

--- setup-test.sh
+++ setup-test.sh
@@ -3,6 +3,7 @@
 SERF=quay.io/phamhongviet/serf
 
 LEADER=`docker run -d -p 127.0.0.1:7373:7373 $SERF agent -rpc-addr '0.0.0.0:7373'`
+echo $LEADER
 LEADER_IP=`docker inspect -f '{{.NetworkSettings.IPAddress}}' $LEADER`
 
 docker run -d $SERF agent -tag role=web -tag dc=cali -tag srv=foo -join $LEADER_IP
@@ -18,5 +19,5 @@ docker run -d $SERF agent -tag role=db -tag dc=oreg -join $LEADER_IP
 DEAD=`docker run -d $SERF agent -tag role=web -tag dc=oreg -tag srv=bar -join $LEADER_IP`
 DEAD="$DEAD `docker run -d $SERF agent -tag role=web -tag dc=cali -tag srv=foo -join $LEADER_IP`"
 sleep 0.5
-docker stop -t 0 $DEAD
-docker rm $DEAD
+docker stop -t 0 $DEAD > /dev/null
+docker rm $DEAD > /dev/null
