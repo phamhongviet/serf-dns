09f772349f7c3701457cd014f01c63b28cce3355
Viet Pham <phamhongviet52@gmail.com>
2016-04-17 06:01:38 +0700

    add usage message

--- config.go
+++ config.go
@@ -13,6 +13,18 @@ var (
 )
 
 func init() {
+	config.Use(configure.NewFlagWithUsage(usage))
 	config.Use(configure.NewEnvironment())
 	config.Use(configure.NewFlag())
+}
+
+func usage() string {
+	return `Usage: serf-dns [options]
+Options:
+-h               This help
+--bind           Bind to interface and port (default: 0.0.0.0:5327)
+--domain-name    Specify domain name (default: serf.)
+--serf           Serf RPC address (default: 127.0.0.1:7373)
+--serf-auth      Serf RPC authentication key (default: empty)
+`
 }
